d4a1696558b4cdc26e185a71a2f113df9f30ce2b
Justin Kenney <justinkenney13@gmail.com>
2018-11-01 18:26:03 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,18 +44,14 @@ class Student
   end
 
   def self.new_from_db(row)
-    binding.pry
-    sql = <<-SQL
-    SELECT * FROM students
-    SQL
-
-
-
+    new_student = Student.new(row[1], row[2], row[0])
   end
 
-  def self.find_by_name
-
-  end
+  def self.find_by_name(name)
+      sql = "SELECT * FROM songs WHERE name = ?"
+      result = DB[:conn].execute(sql, name)[0]
+      Song.new(result[0], result[1], result[2])
+    end
 
   def update
 
