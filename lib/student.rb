eb01e789315d425c61d7edeec9f727b68c3cf3b6
Justin Kenney <justinkenney13@gmail.com>
2018-11-01 18:28:25 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,7 +49,7 @@ class Student
 
   def self.find_by_name(name)
       sql = "SELECT * FROM students WHERE name = ?"
-      
+
       result = DB[:conn].execute(sql, name)[0]
       binding.pry
     end
