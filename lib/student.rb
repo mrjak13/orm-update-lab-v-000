a2b3c6a6571d0b3b5a2da02ade0b812d27b8575d
Justin Kenney <justinkenney13@gmail.com>
2018-11-01 18:05:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,6 +28,26 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def save
+
+  end
+
+  def self.create
+
+  end
+
+  def self.new_from_db
+
+  end
+
+  def self.find_by_name
+
+  end
+
+  def update
+
+  end
+
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
